bfaee26fe46c3177c5c6f7dc01a6ea030e7c2081
Ryan Knight <ryan.knight@evolvemep.com>
2021-09-03 11:27:49 -0600

    setup as remote

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import React from 'react';
 
-import { lazy } from '@loadable/component';
-
-//@ts-ignore
-const Header = lazy(() => import('app2/Header'));
-
-console.log(Header);
-
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Container Application</p>
-        <React.Suspense fallback={<div>Loading Header...</div>}>
-          <Header />
-        </React.Suspense>
+        <p>Remote Application</p>
       </header>
     </div>
   );
